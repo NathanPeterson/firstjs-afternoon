9c5bb7665d6dce88d7456160de2b45c51c722273
Nathan Peterson <nalexpeterson@gmail.com>
2016-06-01 09:58:08 -0400

    Display Color as Color

--- js/app.js
+++ js/app.js
@@ -1,32 +1,30 @@
 $(document).foundation()
 
-document.querySelector('form').onsubmit = function(ev){
-        ev.preventDefault();
-        var details = document.querySelector('div.details');
-        var name =this.firstName
-        var hairColor = this.hairColor
-        var age = this.age
-        var birthplace = this.birthplace
+var addValuesToDetails = function(ev) {
+  ev.preventDefault();
+  var details = document.querySelector('.details');
+  var name = this.name;
+  var hairColor = this.hair_color;
+  var age = this.age;
+  var birthplace = this.birthplace;
 
-        var colorDiv = '<div style="height:100px; width: 100px; background-color: '+hairColor.value+'" >  </div>'
-        // details.innerHTML += "<strong>Name: </strong>" +  + ' | ';
-        // details.innerHTML += "<strong>Hair Color: </strong>" + + " | ";
-        // details.innerHTML += "<strong>Age: </strong>" +  + " | ";
-        // details.innerHTML += "<strong>Birth Place: </strong>" +  + '</p>';
+  var colorDiv = '<div style="height: 100px; width: 100px; background-color: ' + hairColor.value + '"></div>';
 
-        details.innerHTML += ' \
-          <dl>\
-            <dt>Name<dt>\
-            <dd>'+ name.value +'</dd>\
-            \
-            <dt>Hair Color<dt>\
-            <dd>'+ hairColor.value +'</dd>\
-            \
-            <dt>Age<dt>\
-            <dd>'+ age.value +'</dd>\
-            \
-            <dt>Birthplace<dt>\
-            <dd>'+ birthplace.value +'</dd>\
-        </dl>';
+  details.innerHTML += ' \
+    <dl> \
+      <dt>Name</dt> \
+      <dd>' + name.value + '</dd> \
+          \
+      <dt>Hair Color</dt> \
+      <dd>' + colorDiv + '</dd> \
+        \
+      <dt>Age</dt> \
+      <dd>' + age.value + '</dd> \
+        \
+      <dt>Birthplace</dt> \
+      <dd>' + birthplace.value + '</dd> \
+    </dl>';
+};
 
-       };
+
+document.querySelector('form').onsubmit = addValuesToDetails;
