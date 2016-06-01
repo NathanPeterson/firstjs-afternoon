15bd470ae197229d35bbd60ef775628b54280bde
Nathan Peterson <nalexpeterson@gmail.com>
2016-06-01 16:19:58 -0400

    Upload Afternoon Session

--- js/app.js
+++ js/app.js
@@ -1,56 +1,51 @@
 $(document).foundation();
 
-(function(){
-  var buildColorDiv = function(color) {
-    return '<div style="height: 100px; width: 100px; background-color: ' + color + '"></div>'
-  };
+var BasicForm = {
 
-  var buildList = function(listValues = {}){
-      var dl = document.createElement('dl');
-      dl.style.border = '1px solid #999'
-      dl.appendChild(buildListItem('Name', listValues.name))
-      dl.appendChild(buildListItem('color', listValues.color))
-      dl.appendChild(buildListItem('Age', listValues.age))
-      dl.appendChild(buildListItem('Birth Place', listValues.birthplace))
-      return dl;
-  };
+  init: function() {
+    document.querySelector('form').onsubmit = this.addValuesToDetails;
+  },
 
-  var buildListItem = function(term, definition){
+  buildColorDiv: function(color) {
+    if (color === undefined) {
+      color = 'red';
+    }
+    return '<div style="height: 20px; width: 20px; border-radius: 50%;background-color: ' + color + '"></div>';
+  },
+
+  buildList: function(listValues) {
+    var dl = document.createElement('dl');
+    dl.style.border = '1px solid blue';
+    dl.appendChild(this.buildListItem('Name:', listValues.name));
+    dl.appendChild(this.buildListItem('Hair Color:', listValues.hairColor));
+    dl.appendChild(this.buildListItem('Age:', listValues.age));
+    dl.appendChild(this.buildListItem('Birth Place:', listValues.birthplace));
+    return dl;
+  },
+
+  buildListItem: function(term, definition) {
     var li = document.createElement('li');
     var dt = document.createElement('dt');
     var dd = document.createElement('dd');
     dt.innerHTML = term;
     dd.innerHTML = definition;
-    li.appendChild(dt)
-    li.appendChild(dd)
+    li.appendChild(dt);
+    li.appendChild(dd);
     return li;
-  }
+  },
 
-  var addValuesToDetails = function(ev) {
+  addValuesToDetails: function(ev) {
     ev.preventDefault();
     var details = document.querySelector('.details');
-    var hairColor = this.hair_color;
-    var colorDiv = buildColorDiv(hairColor.value);
+    var obj = {
+      name: this.name.value,
+      age: this.age.value,
+      birthplace: this.birthplace.value,
+      hairColor: BasicForm.buildColorDiv(this.hairColor.value)
+    };
 
-    // ANOTHER WAY TO CREATE OBJECT
-    //
-    // var formValues={
-    //   name: his.name.value,
-    //   age: this.age.value,
-    //   birthplace:this.birthplace.value,
-    //   color: colorDiv
-    // };
+    details.appendChild(BasicForm.buildList(obj));
+  }
+};
 
-
-    var object = {};
-    object.name = this.name.value;
-    object.color= colorDiv;
-    object.age = this.age.value;
-    object.birthplace = this.birthplace.value;
-
-    details.appendChild(buildList(object));
-
-  };
-
-  document.querySelector('form').onsubmit = addValuesToDetails;
-})();
+BasicForm.init();
